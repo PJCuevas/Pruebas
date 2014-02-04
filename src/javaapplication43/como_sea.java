644dd23d4567170abfa13f744104a8cf7aee27c8
1-DAM <1-DAM@Aula2-PC01>
2014-02-04 08:47:15 +0100

    Clase creada

---
+++ src/javaapplication43/como_sea.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication43;
+
+/**
+ *
+ * @author 1-DAM
+ */
+public class como_sea {
+    
+}
